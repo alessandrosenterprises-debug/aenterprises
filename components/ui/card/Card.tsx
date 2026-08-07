8678a154f0e47c583e60ac016fb3a72b2e06a58c
Alessandro Enterprises <alessandrosenterprises@gmail.com>
2026-08-07 16:42:10 +0000

    ...

--- components/ui/card/Card.tsx
+++ components/ui/card/Card.tsx
@@ -3,14 +3,17 @@ import { ReactNode } from "react";
 interface CardProps {
   children: ReactNode;
   className?: string;
+  onClick?: () => void;
 }
 
 export default function Card({
   children,
   className = "",
+  onClick,
 }: CardProps) {
   return (
     <div
+      onClick={onClick}
       className={`
         rounded-2xl
         border
